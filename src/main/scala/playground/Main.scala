dd6c7e9088703462747556dd00c93d5543a99afd
Nikki Zareen <nikkizareen@wejoM-2512.local>
2020-02-27 15:19:03 +0000

    declaring some simple values

--- src/main/scala/playground/Main.scala
+++ src/main/scala/playground/Main.scala
@@ -1,5 +1,38 @@
 package playground
 
 object Main extends App {
-    println("> app running")
+
+  def addSomething(x: Int, y: Int): Int =
+    x + y
+
+  val age: Int =
+    100
+
+  val num0: Double =
+    100d
+
+  val str: String =
+    "Hello"
+
+  val character: Char =
+    'A'
+
+  val lng: Long =
+    2L
+
+  val listA: List[Int] =
+    List(1, 2, 3, 4, 5)
+
+  val listB: List[String] =
+    List("a", "b", "c")
+
+  println(addSomething(age, 2))
+  println("nikki")
+  println(age)
+  println(num0)
+  println(str)
+  println(character)
+  println(lng)
+  println(listA)
+  println(listB)
 }
